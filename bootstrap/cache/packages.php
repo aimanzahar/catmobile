79d84aca5913898c8c8a83a937499fc2dd1a3e1b
Zahar <anthonymuncher@gmail.com>
2026-05-18 21:42:54 +0800

    feat: remove CameraServiceProvider from package and service registrations

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -20,13 +20,6 @@
       0 => 'Native\\Mobile\\NativeServiceProvider',
     ),
   ),
-  'nativephp/mobile-camera' => 
-  array (
-    'providers' => 
-    array (
-      0 => 'Native\\Mobile\\Providers\\CameraServiceProvider',
-    ),
-  ),
   'nesbot/carbon' => 
   array (
     'providers' => 
